6a411846203c91c0584b5bc24bf08953c94cc78a
Dylan Jenkins <dylanljenkins@gmail.com>
2024-09-02 21:00:34 +1000

    Comments and cleanup

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    // This builds as a full SPA. We may not want this if we do more advanced things like auth/dbs in future.
+    // output: "export"
+};
 
 export default nextConfig;
